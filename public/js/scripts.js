a76849129f8f7051d18ceb4388260dcc33e48d47
PSUser <juanpablochoter@gmail.com>
2024-02-08 09:09:33 -0300

    links in chrome

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,14 @@
 const linksBtns = document.querySelectorAll(".linksBtn")
 
+window.addEventListener("load", (e)=>{
+    if (navigator.userAgent.indexOf("Chrome") > -1) {
+        document.querySelectorAll(".linksBtn").forEach(link=>{
+            link.style.transform = "rotate(180deg)"
+        })
+    }
+})
+
+
 let glitchAnimation;
 linksBtns.forEach(btn=>{
     btn.addEventListener("mouseover", (e)=>{
@@ -30,88 +39,3 @@ modalsLinksList.forEach(modal=>{
         })
     })
 })
-
-
-
-
-// setInterval(updateImage2, 50);
-
-
-// window.addEventListener("load", () => {
-
-//     let country = document.querySelector("#id_ciudad_de_entrega");
-//     let deliveryAdress = document.querySelector("#id_direccion_de_entrega_calle_numero_localidad_cp");
-//     const checkBoxPoliticas = document.querySelector('input[name="acepto_las_politicas_de_privacidad"]')
-//     checkBoxPoliticas.checked = true;
-//     checkBoxPoliticas.parentElement.parentElement.style.display = "none"
-
-//     const dateHelpText = document.querySelector('input[name="fecha_de_entrega_se_muestran_solo_los_disponibles"]')
-//     dateHelpText.nextElementSibling.style.fontWeight = "bold"
-    
-//     country.addEventListener("change", () => {
-//         if (country.value == "Buenos Aires Of Puerto Madero" || country.value == "Buenos Aires Of Pilar") {
-//             deliveryAdress.parentElement.classList.add("d-none");
-//             document.querySelector("input[name='direccion_de_entrega_calle_numero_localidad_cp']").value = "None"
-//             $('input[name="fecha_de_entrega_se_muestran_solo_los_disponibles"]').datepicker().data('datepicker').setStartDate(`+1d`);
-//         }else if(country.value == "Buenos Aires Otra locación"){
-//             document.querySelector('input[name="ciudad_de_entrega_other"]').disabled = true;
-//         }else {
-//             deliveryAdress.parentElement.classList.remove("d-none");
-//             document.querySelector("input[name='direccion_de_entrega_calle_numero_localidad_cp']").value = "";
-//             $('input[name="fecha_de_entrega_se_muestran_solo_los_disponibles"]').datepicker().data('datepicker').setStartDate(`+7d`);
-//         };
-//     });
-
-//     // DATEPICKER HOLIDAYS
-//     const ciudadEntrega = document.querySelector("#id_ciudad_de_entrega");
-    
-//     ciudadEntrega.addEventListener("change", async (e) => {
-//         const country = getCountryCode(e.target.value);
-//         if(country){
-//             const holidayList = await getHolidays(country);
-//             disableDates(holidayList);
-//         }
-//     });
-  
-//     const getCountryCode = (city) => {
-//         switch (city) {
-//             case "Bogotá" || "Medellín":
-//                 return "CO";
-//             case "Lima":
-//                 return "PE";
-//             case "Santiago":
-//                 return "CL";
-//             case "Otro":
-//                 return null
-//         }
-
-//         if (city.includes("Buenos Aires")){
-//             return "AR"
-//         }
-//     };
-  
-//     const getHolidays = async (country) => {
-//         const API_KEY = "AIzaSyAqHxbFTYlIkxP_u6ihKJ6Qysck6QGPyJg";
-//         const idioma = "es";
-//         const url = `https://www.googleapis.com/calendar/v3/calendars/${idioma}.${country}%23holiday%40group.v.calendar.google.com/events?key=${API_KEY}`;
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         const holidayList = data.items
-//             .filter((item) =>!item.description.includes("Para ocultar las celebraciones"))
-//                 .map((item) => item.start.date)
-//                 .map((date) => date.split("-").reverse().join("-"));
-        
-//         console.log("holidays ok!");
-//         return holidayList;
-//     };
-  
-//     const disableDates = (holidayList) => {
-//         const datepicker = document.querySelector('input[name="fecha"]');
-//         console.log(holidayList)
-//         if (datepicker) {
-//             $('input[name="fecha"]')
-//                 .datepicker().data('datepicker').setDatesDisabled(holidayList);
-//         }
-//     };
-
-// });
